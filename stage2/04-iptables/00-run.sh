d1aecbaa70d7a7bdcff79a90c23ca201b2c968b2
Bill Zimmerman <billzimmerman@gmail.com>
2025-02-23 23:53:47 +0100

    Update 00-run.sh

--- stage2/04-iptables/00-run.sh
+++ stage2/04-iptables/00-run.sh
@@ -6,8 +6,11 @@ apt-get update
 apt-get install -y iptables iptables-persistent netfilter-persistent
 update-alternatives --set iptables /usr/sbin/iptables-legacy
 update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+update-alternatives --display iptables
 
+mkdir -p /etc/iptables
 touch /etc/iptables/rules.v4 /etc/iptables/rules.v6
 chmod 644 /etc/iptables/rules.v4 /etc/iptables/rules.v6
 
 systemctl enable netfilter-persistent
+iptables --version
