99ef5415ba3b0bb748a8e7c88e7b1b8920c7d0d8
Bill Zimmerman <billzimmerman@gmail.com>
2025-02-28 18:49:46 +0100

    Remove debug log write

--- stage2/04-iptables/00-run.sh
+++ stage2/04-iptables/00-run.sh
@@ -3,21 +3,19 @@
 
 set -x  # Enable debugging
 
-LOG_FILE="${ROOTFS_DIR}/var/log/iptables-setup.log"
-echo "Starting iptables setup" | tee -a "$LOG_FILE"
+echo "Starting iptables setup"
 
-# Install firewall config script and service
+# Install routing config script and service
 install -m 755 files/raspap-routing.sh "${ROOTFS_DIR}/usr/local/bin/raspap-routing.sh"
 install -m 644 files/routing.service "${ROOTFS_DIR}/etc/systemd/system/routing.service"
 
 # Verify installation
-ls -l "${ROOTFS_DIR}/usr/local/bin/" | tee -a "$LOG_FILE"
-ls -l "${ROOTFS_DIR}/etc/systemd/system/" | tee -a "$LOG_FILE"
+ls -l "${ROOTFS_DIR}/usr/local/bin/"
+ls -l "${ROOTFS_DIR}/etc/systemd/system/"
 
 # Enable IP routing on first boot
 on_chroot << EOF
 systemctl enable routing.service 2>&1 | tee -a "$LOG_FILE"
 EOF
 
-echo "Finished iptables setup" | tee -a "$LOG_FILE"
-
+echo "Finished iptables setup"
